c0b7c0e72e7651df0a9c4f9e6afff2f865a9e9db
renasboy <renasboy@gmail.com>
2012-07-12 01:02:18 +0200

    Fixed https urls in the mails

--- usr/app/template/mail_store_shop_confirm.tpl.php
+++ usr/app/template/mail_store_shop_confirm.tpl.php
@@ -1,7 +1,7 @@
-<?php $link = sprintf('https//%s/%s/confirm/%d/%s', $conf->get('base_host'), $name, $shop_id, $token); ?>
+<?php $link = sprintf('http://%s/%s/confirm/%d/%s', $conf->get('base_host'), $name, $shop_id, $token); ?>
 <p>Hi there,</p>
 
-<p>Your email address was used to place an order at the store <a href="<?php print sprintf('https//%s/%s', $conf->get('base_host'), $name); ?>"><?php print $name; ?></a> hosted at <a href="http://<?php print $conf->get('base_host'); ?>">canistro, your personal e-commerce</a>.</p>
+<p>Your email address was used to place an order at the store <a href="<?php print sprintf('http://%s/%s', $conf->get('base_host'), $name); ?>"><?php print $name; ?></a> hosted at <a href="http://<?php print $conf->get('base_host'); ?>">canistro, your personal e-commerce</a>.</p>
 
 <p>If this order was indeed placed by you, please follow this <a href="<?php print $link; ?>">link</a> to confirm it.</p>
 
