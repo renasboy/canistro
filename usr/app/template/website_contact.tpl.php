c46ac85d0f10e045ea5271f0b1395daaa3f83335
renasboy <renasboy@gmail.com>
2012-07-12 22:17:02 +0200

    Updated email to the canistro one

--- usr/app/template/website_contact.tpl.php
+++ usr/app/template/website_contact.tpl.php
@@ -6,7 +6,7 @@
                     <p>Canistro is built based on open source development and your feedback is very much appreciated.</p>
                     
                     <h2>email</h2>
-                    <p>r<!-- NOSPAM -->e<!-- NOSPAM -->n<!-- NOSPAM -->a<!-- NOSPAM -->s<!-- NOSPAM -->b<!-- NOSPAM -->o<!-- NOSPAM -->y<!-- NOSPAM --><!-- NOSPAM -->@<!-- NOSPAM -->g<!-- NOSPAM -->m<!-- NOSPAM -->a<!-- NOSPAM -->i<!-- NOSPAM -->l<!-- NOSPAM -->.<!-- NOSPAM -->c<!-- NOSPAM -->o<!-- NOSPAM -->m</p>
+                    <p>c<!-- NOSPAM -->a<!-- NOSPAM -->n<!-- NOSPAM -->i<!-- NOSPAM -->s<!-- NOSPAM -->t<!-- NOSPAM -->r<!-- NOSPAM -->o<!-- NOSPAM -->.<!-- NOSPAM -->o<!-- NOSPAM -->n<!-- NOSPAM -->l<!-- NOSPAM -->i<!-- NOSPAM -->n<!-- NOSPAM -->e<!-- NOSPAM -->@<!-- NOSPAM -->g<!-- NOSPAM -->m<!-- NOSPAM -->a<!-- NOSPAM -->i<!-- NOSPAM -->l<!-- NOSPAM -->.<!-- NOSPAM -->c<!-- NOSPAM -->o<!-- NOSPAM -->m</p>
                     <h2>github</h2>
                     <p>github.com/renasboy/canistro</p>
                     <h2>address</h2>
